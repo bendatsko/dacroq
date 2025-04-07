c185140fc69106e2dbdf7de5c7edf050158db068
Ben Datsko <bldatsko@gmail.com>
2025-04-07 14:07:22 -0400

    purge

--- dacroq_web/public/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-// Fix the TypeScript error by adding a type to props
-const SVGComponent = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    id="Layer_1"
-    xmlns="http://www.w3.org/2000/svg"
-  />
-);
