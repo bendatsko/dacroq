708f4c1328db9fcc65fcf8b85fc818963a978d36
bdatsko <bdatsko@umich.edu>
2025-05-29 22:24:23 -0400

    stuff

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 BASE_PATH=$(pwd)
 
+# need to add 
+    # sudo apt-get install nodejs 
+    # npm install -g pnpm
+
+sudo apt-get install nodejs 
+sudo apt-get install npm
+sudo apt-get install python3
+npm install -g pnpm
+
 cd "${BASE_PATH}/daemon"
 pnpm install
 
--- start-daemon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd daemon || exit
-python3 app.py 
